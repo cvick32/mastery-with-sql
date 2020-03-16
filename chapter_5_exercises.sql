b8d39f6b0ad44f5196a1e5e51dc4d433b5c96001
cole <cole.d.vick@gmail.com>
2020-03-15 23:27:16 -0400

    start ch 5

---
+++ chapter_5_exercises.sql
@@ -0,0 +1,14 @@
+-- 5.1 Write a query to print a description of each film's length as 
+-- shown in the output below. When a film does not have a length, 
+-- print: [title] is unknown length
+
+select title || ' is ' || coalesce(length || ' minutes', 'unknown length') as length_desc
+from film;
+
+
+-- 5.2 You want to play a movie title guessing game with some friends. 
+-- Write a query to print only the first 3 letters of each film title 
+-- and then '*' for the rest (The repeat function may come in handy here...)
+
+select left(title, 3) || repeat('*', length(title) - 3) as "Guess!"
+from film;
